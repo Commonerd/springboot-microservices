f229950ea0d5195f1c986fffc55964ec620a400e
[Commonerd] <[syh712@gmail.com]>
2023-10-04 11:22:36 +0900

    feat : Add REST API Documentation for organization service

--- organization-service/src/main/java/net/javaguides/organizationservice/controller/OrganizationController.java
+++ organization-service/src/main/java/net/javaguides/organizationservice/controller/OrganizationController.java
@@ -1,5 +1,8 @@
 package net.javaguides.organizationservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.javaguides.organizationservice.dto.OrganizationDto;
 import net.javaguides.organizationservice.service.OrganizationService;
@@ -7,6 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Organization Service - EmployeeController"
+
+)
 @RestController
 @RequestMapping("api/organizations")
 @AllArgsConstructor
@@ -15,6 +22,14 @@ public class OrganizationController {
     private OrganizationService organizationService;
 
 
+    @Operation(
+            summary = "Save Organization REST API",
+            description = "Save Organization REST API is used to save Organization object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     // Build Save Organization REST API
     @PostMapping
     public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
@@ -22,6 +37,14 @@ public class OrganizationController {
         return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Organization REST API",
+            description = "Get a Organization REST API is used to save Organization object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     // Build Get Organization REST API
     @GetMapping("{code}")
     public ResponseEntity<OrganizationDto> getOrganizationByCode(@PathVariable("code") String organizationCode) {
--- organization-service/src/main/java/net/javaguides/organizationservice/dto/OrganizationDto.java
+++ organization-service/src/main/java/net/javaguides/organizationservice/dto/OrganizationDto.java
@@ -1,6 +1,7 @@
 package net.javaguides.organizationservice.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,14 +9,29 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        description = "Organization Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrganizationDto {
     private Long id;
+    @Schema(
+            description = "Organization Name"
+    )
     private String organizationName;
+    @Schema(
+            description = "Organization Description"
+    )
     private String organizationDescription;
+    @Schema(
+            description = "Organization Code"
+    )
     private String organizationCode;
+    @Schema(
+            description = "Organization Created Date"
+    )
     private LocalDateTime createdDate;
 }
