99e98000cf25c153aabdd926c00c6be44c8013c2
[Commonerd] <[syh712@gmail.com]>
2023-10-04 11:15:42 +0900

    feat : Add REST API documentation for employee service

--- employee-service/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,10 @@
 package net.javaguides.employeeservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -8,6 +13,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "employee Service REST APIs",
+				description = "employee Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "ahahaha",
+						email = "abc@gmail.com",
+						url = "https://www.abc.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.abc.net"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "employee-Service Doc",
+				url = "https://www.abc.com"
+		)
+)
 @SpringBootApplication
 @EnableFeignClients
 public class EmployeeServiceApplication {
--- employee-service/employee-service/src/main/java/net/javaguides/employeeservice/dto/EmployeeDto.java
+++ employee-service/employee-service/src/main/java/net/javaguides/employeeservice/dto/EmployeeDto.java
@@ -1,20 +1,39 @@
 package net.javaguides.employeeservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "Employee Model Information"
+)
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDto {
     private Long id;
+    @Schema(
+            description = "First Name"
+    )
     private String firstName;
+    @Schema(
+            description = "Last Name"
+    )
     private String lastName;
+    @Schema(
+            description = "Email"
+    )
     private String email;
+    @Schema(
+            description = "Department Code"
+    )
     private String departmentCode;
+    @Schema(
+            description = "Organization Code"
+    )
     private String organizationCode;
 
 
